6ffa31ad33e6340e493ce6b3917a347245f65a78
Jeremiah Ang <jeremiah_ang@sp.edu.sg>
2022-09-08 13:18:16 +0800

    Fetch recursive

--- public/retrieve/index.js
+++ public/retrieve/index.js
@@ -16,6 +16,18 @@ function fetchModule(code) {
     });
 }
 
+function fetchModuleRecursive(codes, index, results) {
+    if (index >= codes.length) return results;
+    // No more modules to fetch. Stop calling yourself, and just return the accumulated results
+
+    const code = codes[index];
+    return fetchModule(code).then(function (result) {
+        // Make the next fetch after receiving response
+        results.push(result); // accumulate result
+        return fetchModuleRecursive(codes, index + 1, results); // index + 1 to process next module
+    });
+}
+
 window.addEventListener('DOMContentLoaded', function () {
     const form = document.querySelector('form'); // Only have 1 form in this HTML
 
@@ -30,7 +42,8 @@ window.addEventListener('DOMContentLoaded', function () {
     document.querySelector('#retrieve').onclick = function () {
         const rows = document.querySelectorAll('tbody tr');
 
-        // TODO: Use 1 query instead of row.length number of queries.
+        // Extract codes first
+        const codes = [];
         for (let i = 0; i < rows.length; i++) {
             const row = rows[i];
             const code = row.querySelector('td:first-child').textContent;
@@ -39,17 +52,28 @@ window.addEventListener('DOMContentLoaded', function () {
 
             nameCell.textContent = 'Loading...';
             creditCell.textContent = 'Loading...';
-            fetchModule(code)
-                .then(function (body) {
-                    if (body.error) throw new Error(body.error);
-                    nameCell.textContent = body.module.name;
-                    creditCell.textContent = body.module.credit;
-                })
-                .catch(function (error) {
-                    nameCell.textContent = 'XXXXXXXX';
-                    creditCell.textContent = error.message;
-                });
+
+            codes.push(code);
         }
+
+        // Send request for one code at a time
+        fetchModuleRecursive(codes, 0, []).then(function (results) {
+            // Process all the result
+            for (let i = 0; i < results.length; i++) {
+                const result = results[i];
+                const row = rows[i];
+                const nameCell = row.querySelector('td:nth-child(2)');
+                const creditCell = row.querySelector('td:nth-child(3)');
+                if (!result.error) {
+                    // no error
+                    nameCell.textContent = result.module.name;
+                    creditCell.textContent = result.module.credit;
+                } else {
+                    nameCell.textContent = 'XXXXXXXX';
+                    creditCell.textContent = result.error;
+                }
+            }
+        });
     };
 
     // Calculating GPA
